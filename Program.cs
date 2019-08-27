4d18d1324ffc5f4967e36b14cee4643d0d854786
milomacphail <milomacphail@gmail.com>
2019-08-27 19:36:55 -0400

    Version 1.3 - Deck of cards print and does QualityCheck;

--- Program.cs
+++ Program.cs
@@ -4,84 +4,125 @@ using System.Collections.Generic;
 
 namespace DiceRacer
 {
-
-    class Dice
+    class Card
     {
-        Random rand = new Random();
-        int value;
-
-        public Dice()
-        {
-            this.value = this.rollDice();
-        }
-
-        public int rollDice()
-        {
-            return rand.Next(1, 6);
-        }
-
-    }
-    class Deck
-    {
-        public List<Card> newDeck = new List<Card>();
-
-        public List<Card> CreateDeck()
-        {
-            for (int face = 2; face <= 14; face++)
-            {
-                for (int suits = 0; suits <= 3; suits++)
-                {
-                    Card card = new Card(face, suits);
-                    newDeck.Add(card);
-                }
-            }
-
-            return newDeck;
-        }
-
-
-        public Card Shuffle()
-        {
-
-        }
-
-    }
-
-    public class Card
-    {
-        public enum SuitValues { Clubs = 0, Diamonds = 1, Hearts = 2, Spades = 4 };
+        public enum SuitValues { Clubs = 0, Diamonds = 1, Hearts = 2, Spades = 3 };
         Array suits = Enum.GetValues(typeof(SuitValues));
-        SuitValues randomSuitValue = (SuitValues)(new Random()).Next(0, 3);
-
-        Random randomCardValue = new Random();
+        SuitValues SuitValue = (SuitValues)(new Random()).Next(0, 4);
 
         public int CardValue;
         public string CardSuit;
 
+        Random value = new Random();
+
         public Card()
         {
-            this.CardValue = randomCardValue.Next( 1, 14);
-            this.CardSuit = randomSuitValue.ToString();
+            this.CardValue = value.Next(1, 7);
+            this.CardSuit = SuitValue.ToString();
         }
 
         public Card(int CardValue, int CardSuit)
         {
             this.CardValue = CardValue;
-            this.CardSuit = randomSuitValue.ToString();
+            this.CardSuit = SuitValue.ToString();
         }
 
         public Card(int CardValue)
         {
-            this.CardValue = randomCardValue.Next(1, 14);
+            this.CardValue = value.Next(1, 7);
         }
+
+
     }
+
+    class Deck
+    {
+        public List<Card> deck = new List<Card>();
+
+        public Deck()
+        {
+            for (int face = 1; face <= 7; face++)
+            {
+                for (int suits = 0; suits <= 3; suits++)
+                {
+                    Card card = new Card(face, suits);
+                    deck.Add(card);
+                }
+            }
+        }
+
+        /*public IEnumerable<Card> Shuffle()
+        {
+           Random random = new Random();
+           IOrderedEnumerable<Card> shuffled = deck.OrderBy(Card => random.Next());
+           return shuffled;
+        }*/
+
+        public List<Card> Shuffle()
+        {
+            Random random = new Random();
+            IOrderedEnumerable<Card> shuffled = deck.OrderBy(Card => random.Next());
+            List<Card> shuffledList = shuffled.ToList();
+            return shuffledList;
+        }
+
+        public List<Card> QualityCheck()
+        {
+            int listLength = deck.Count();
+
+            if (deck[0].CardValue == 1 || deck[0].CardValue == 2 || deck[0].CardValue == 6 || deck[0].CardValue == 7 ||
+                deck[1].CardValue == 1 || deck[1].CardValue == 2 || deck[1].CardValue == 6 || deck[1].CardValue == 7 ||
+                deck[listLength].CardValue == 1 || deck[listLength].CardValue == 2 || deck[listLength].CardValue == 6 || deck[listLength].CardValue == 7 ||
+                deck[listLength - 1].CardValue == 1 || deck[listLength - 1].CardValue == 2 || deck[listLength - 1].CardValue == 6 || deck[listLength - 1].CardValue == 7)
+            {
+                Shuffle();
+            }
+
+                return deck;
+        }
+
+    }
+
+    class Dice
+        {
+            Random rand = new Random();
+            int value;
+
+            public Dice()
+            {
+                this.value = this.rollDice();
+            }
+
+            public int rollDice()
+            {
+                return rand.Next(1, 6);
+            }
+
+        }
+
+
+    class Output
+        {
+            public static void ConsoleIt(dynamic deck)
+            {
+                foreach (Card item in deck)
+                {
+                  Console.WriteLine("Card:   {0} of {1}", item.CardValue, item.CardSuit.ToString()); ;
+                }
+
+                //Console.WriteLine("Total Cards:  {0}");
+            }
+        }
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Deck newDeck = new Deck();
+            var shuffleDeck = newDeck.Shuffle();
+
+            Output.ConsoleIt(shuffleDeck);
         }
     }
 
